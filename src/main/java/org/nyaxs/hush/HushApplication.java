2fd618bda2c697647a699f33da24fce99f532f2f
nyaxs <nyaxs@outlook.com>
2020-08-05 14:21:12 +0800

    first commit

---
+++ src/main/java/org/nyaxs/hush/HushApplication.java
@@ -0,0 +1,13 @@
+package org.nyaxs.hush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HushApplication.class, args);
+    }
+
+}
